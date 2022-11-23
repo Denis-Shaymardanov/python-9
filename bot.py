9b9e650b8849883c4b3f11f576cf2019f1af1fa1
Denis Shaymardanov <d9655352866@gmail.com>
2022-11-23 18:44:36 +0500

    added new functions

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import weather as w
+import weather as weather
+import log
 
 from config import BOT_TOKEN
 
@@ -10,19 +11,31 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\n Этот бот поможет тебе узнать погоду в интересующем тебя городе")
+    await message.reply("Привет!\n Этот бот поможет тебе узнать текущую погоду или прогноз погоды в интересующем тебя городе")
+    log.write(message.from_user, 'bot is started')
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши /weather <Название города>, чтобы узнать погоду в интересущем тебя городе")
+    await message.reply("Напиши /temperature <Название города>, чтобы узнать погоду в интересущем тебя городе\n Напиши /tomorrow <Название города>, чтобы узнать прогноз погоды на завтра в интересущем тебя городе")
+    log.write(message.from_user, 'ask for help')
 
-@dp.message_handler(commands=['weather'])
+@dp.message_handler(commands=['temperature'])
 async def weather_message(message: types.Message):
     argument = message.get_args()
     if not argument:
         argument = 'Yekaterinburg'
-    answer = "Температура на улице %s" % (w.current_weather(argument))
+    answer = "Температура на улице %s" % (weather.current_temperature(argument))
     await bot.send_message(message.from_user.id, answer)
+    log.write(message.from_user, 'ask for temperature')
+
+@dp.message_handler(commands=['tomorrow'])
+async def weather_message(message: types.Message):
+    argument = message.get_args()
+    if not argument:
+        argument = 'Yekaterinburg'
+    answer = "Температура на завтра в это же время %s" % (weather.tomorrow_temperature(argument))
+    await bot.send_message(message.from_user.id, answer)
+    log.write(message.from_user, 'ask for tomorrow temperature')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
--- weather.py
+++ weather.py
@@ -1,13 +1,18 @@
 from config import WEATHER_KEY
 from pyowm import OWM
+from pyowm.utils import timestamps
 
-def current_weather(city):
-    owm = OWM(WEATHER_KEY)
-    mgr = owm.weather_manager()
+owm = OWM(WEATHER_KEY)
+mgr = owm.weather_manager()
+
+def current_temperature(city):
     observation = mgr.weather_at_place(f'{city},RU')
     w = observation.weather
-    temperature = w.temperature('celsius') 
     
-    for key, value in temperature.items():
-        if key=="temp":
-            return value
+    return w.temperature('celsius')['temp']
+
+def tomorrow_temperature(city):
+    forecast = mgr.forecast_at_place(f'{city},RU', '3h')
+    answer = forecast.get_weather_at(timestamps.tomorrow())
+
+    return answer.temperature('celsius')['temp']
